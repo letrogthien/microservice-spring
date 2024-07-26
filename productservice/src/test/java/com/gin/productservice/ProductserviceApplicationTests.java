e8d7e05d396b043f15754d53c2032fbdf94fec44
le trong thien <letrogthien@gmail.com>
2024-07-26 08:09:33 +0700

    2

---
+++ productservice/src/test/java/com/gin/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gin.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
